b50f8d8eef8d79e7a3606a4df669af4da465f10f
mlescure <mlescure@mlesucre.fr>
2021-09-25 14:32:00 +0200

    add

--- api/admin.py
+++ api/admin.py
@@ -1,72 +1,158 @@
-# from django.contrib import admin
-# from .models import Client, Contract, Team, Event, User
-
-
-# admin.site.register(Client)
-# admin.site.register(Contract)
-# admin.site.register(Team)
-# admin.site.register(User)
-# admin.site.register(Event)
-
 from django.contrib import admin
 from .models import Client, Contract, Team, User, Event
-
+from django.core.exceptions import PermissionDenied
+from django.contrib import messages
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    fieldsets = (
+
+    fieldsets = [
         (
-            None,
+            "Client",
             {
-                 'fields': (
-                    'first_name',
-                    'last_name',
-                    'email',
-                    'phone',
-                    'company_name',
-                    # 'date_created',
-                    # 'date_updated',
-                    'seller_contact'
-                 ),
+                "fields": [
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "phone",
+                    "company_name",
+                    "seller_contact"
+                ]
+            },
+        ),
+        (
+            "Dates",
+            {
+                "fields": ["date_created", "date_updated"],
+            },
+        ),
+    ]
 
-            }),
-    )
-    readonly_fields = ('date_created', 'date_updated')
+    readonly_fields = ["date_created", "date_updated", "seller_contact"]
 
-    def get_readonly_fields(self, request, obj=None):
-        return self.readonly_fields
+    def get_fieldsets(self, request, obj=None):
+        try:
+            team = Team.objects.get(user=request.user)
+            if team.role == 'MANAGER' or request.user.is_superuser():
+                self.readonly_fields.remove('seller_contact')
+                return self.fieldsets
+        except:
+           return self.fieldsets
 
 
-
-    # Permet d'avoir une autorisation d'acceder au panel -> staff_status -> True
     def has_add_permission(self, request):
         return True
 
-    #permet d'afficher le module Client
     def has_module_permission(self, request):
         return True
 
-    # GET
+    #GET
     def has_view_permission(self, request, obj=None):
         return True
 
     #PUT
     def has_change_permission(self, request, obj=None):
-        return True
+        try:
+            team = Team.objects.get(user=request.user)
+            if team.role == 'MANAGER' or obj.seller_contact == team:
+                return True
+        except:
+          return False
+
     #DELETE
     def has_delete_permission(self, request, obj=None):
-        return True
+        try:
+            team = Team.objects.get(user=request.user)
+            if team.role == 'MANAGER' or obj.seller_contact == team:
+                return True
+        except:
+            return False
+    #todo add foreingn key
 
-
-
-
+    def save_model(self, request, obj, form, change):
+        team = Team.objects.get(user=request.user)
+        if team.role == 'SELLER':
+            obj.seller_contact = team
+            obj.save()
 
 
 
 @admin.register(Contract)
 class ContractAdmin(admin.ModelAdmin):
-    amount = 'contract__amount'
-    pass
+
+    fieldsets = [
+        (
+            "Contract",
+            {
+                "fields": [
+                    "title",
+                    "client",
+                    "detail_event",
+                    "amount",
+                    "status",
+                    "seller_contact"
+                ]
+            },
+        ),
+        (
+            "Dates",
+            {
+                "fields": ["created_time"],
+            },
+        ),
+    ]
+
+    readonly_fields = ["created_time", "seller_contact"]
+
+    def get_fieldsets(self, request, obj=None):
+        try:
+            team = Team.objects.get(user=request.user)
+            if team.role == 'MANAGER' or request.user.is_superuser():
+                self.readonly_fields.remove('seller_contact')
+                return self.fieldsets
+        except:
+           return self.fieldsets
+
+
+    def has_add_permission(self, request):
+        return True
+
+    def has_module_permission(self, request):
+        return True
+
+    #GET
+    def has_view_permission(self, request, obj=None):
+        return True
+
+    #PUT
+    def has_change_permission(self, request, obj=None):
+        try:
+            team = Team.objects.get(user=request.user)
+            if team.role == 'MANAGER' or obj.seller_contact == team:
+                return True
+        except:
+            return False
+
+    #DELETE
+    def has_delete_permission(self, request, obj=None):
+        try:
+            team = Team.objects.get(user=request.user)
+            if team.role == 'MANAGER' or obj.seller_contact == team:
+                return True
+        except:
+            return False
+
+    def save_model(self, request, obj, form, change):
+        team = Team.objects.get(user=request.user)
+        client = Client.objects.get(pk=obj.client.id)
+        if client.seller_contact == team or team.role == 'MANGER':
+            obj.seller_contact = team
+            obj.save()
+        else:
+            return messages.error(request, "You can't")
+
+
+
 
 
 @admin.register(Event)
